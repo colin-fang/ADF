205450e2267ef1ae3faec60043af1c959b5e40ed
colin-fang <46767117+colin-fang@users.noreply.github.com>
2024-08-22 11:28:57 -0400

    gitignore and some name changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
-const ADF = artifacts.require('ADFImplementation');
+const UTC = artifacts.require('UTCImplementation');
 const Proxy = artifacts.require('AdminUpgradeabilityProxy');
 
 module.exports = async function(deployer) {
   await deployer;
 
-  await deployer.deploy(ADF);
-  const proxy = await deployer.deploy(Proxy, ADF.address);
-  const proxiedADF = await ADF.at(proxy.address);
+  await deployer.deploy(UTC);
+  const proxy = await deployer.deploy(Proxy, UTC.address);
+  const proxiedUTC = await UTC.at(proxy.address);
   await proxy.changeAdmin("0x417A8676ede1D52e63C913F051c169a863c018a0");
-  await proxiedADF.initialize();
+  await proxiedUTC.initialize();
 };
